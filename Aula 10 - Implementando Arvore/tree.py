44da6ecef2d53202af9eaab6b63980b5cf795c07
Adriel Klem <adrielklem16@gmail.com>
2023-03-12 16:16:32 -0300

    update

--- Aula 10 - Implementando Arvore/tree.py
+++ Aula 10 - Implementando Arvore/tree.py
@@ -12,3 +12,13 @@ class BinaryTree:
     def __init__(self, data):
         node = TreeNode(data)
         self.root = node
+
+
+if __name__ == "__main__":
+    tree = BinaryTree(7)
+    tree.root.left = TreeNode(18)
+    tree.root.right = TreeNode(14)
+
+    print(tree.root)
+    print(tree.root.right)
+    print(tree.root.left)
